53c549be4fa4da803b54806624ad1193853ad600
suganya140511 <suganya14.cse@gmail.com>
2026-04-26 00:03:35 +0530

    Add files via upload
    Commit

---
+++ OddorEven.js
@@ -0,0 +1,14 @@
+console.log("Print the given number is odd or even")
+function isOddOrEven()
+{
+    let n=25
+    if(n%2==0)
+    {
+        console.log("Given number is even");
+    }
+    else
+    {
+        console.log("Given number is odd");
+    }
+}
+isOddOrEven()
